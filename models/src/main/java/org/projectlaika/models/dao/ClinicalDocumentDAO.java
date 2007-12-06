1703e528ffd14d8f49e4d03674978c30a5bfb328
Andy Gregorowicz <andrewg@mitre.org>
2007-12-06 21:58:40 +0000

    Starting to build DAO's to access the models

---
+++ models/src/main/java/org/projectlaika/models/dao/ClinicalDocumentDAO.java
@@ -0,0 +1,69 @@
+package org.projectlaika.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.projectlaika.models.ClinicalDocument;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Data access object for ClinicalDocuments
+ * @author Andy Gregorowicz
+ */
+@Transactional
+public class ClinicalDocumentDAO
+{
+    private EntityManager entityManager;
+    
+    /**
+     * Gets a list of all of the ClinicalDocuments stored in Laika
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public List<ClinicalDocument> findAll()
+    {
+        Query query = entityManager.createQuery("select cd from ClinicalDocument cd");
+        List<ClinicalDocument> docs = query.getResultList();
+        return docs;
+    }
+    
+    /**
+     * Finds a ClinicalDocument specified by the id
+     * @param id
+     * @return
+     */
+    public ClinicalDocument find(int id)
+    {
+        return entityManager.find(ClinicalDocument.class, id);
+    }
+    
+    /**
+     * Deletes a ClinicalDocument from the database with the id specified
+     * @param id
+     */
+    public void delete(int id)
+    {
+        ClinicalDocument cd = entityManager.find(ClinicalDocument.class, id);
+        entityManager.remove(cd);
+    }
+    
+    /**
+     * Saves the ClinicalDocument to the database. After this method is called, the ClinicalDocument
+     * instance that was passed in will contain the id that it recieved when it was inserted into
+     * the database.
+     * @param cd
+     */
+    public void save(ClinicalDocument cd)
+    {
+        entityManager.persist(cd);
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager)
+    {
+        this.entityManager = entityManager;
+    }
+}
---
+++ models/src/test/java/org/projectlaika/models/dao/ClinicalDocumentDAOTest.java
@@ -0,0 +1,84 @@
+package org.projectlaika.models.dao;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.Is.is;
+
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.projectlaika.models.ClinicalDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+@ContextConfiguration(locations={"spring.xml"})
+public class ClinicalDocumentDAOTest extends AbstractTransactionalJUnit4SpringContextTests
+{
+    private ClinicalDocumentDAO clinicalDocumentDAO;
+
+    @Autowired
+    public void setClinicalDocumentDAO(ClinicalDocumentDAO clinicalDocumentDAO)
+    {
+        this.clinicalDocumentDAO = clinicalDocumentDAO;
+    }
+    @Before
+    public void setUpDB()
+    {
+        deleteFromTables("CLINICALDOCUMENT");
+        simpleJdbcTemplate.update("insert into CLINICALDOCUMENT (id, name, xmlContent) values (?, ?, ?)",
+                5000, "Test Patient 1", "<ClinicalDocument>medical data</ClinicalDocument>");
+        simpleJdbcTemplate.update("insert into CLINICALDOCUMENT (id, name, xmlContent) values (?, ?, ?)",
+                5001, "Test Patient 2", "<ClinicalDocument>more medical data</ClinicalDocument>");
+    }
+    
+    @Test
+    public void testFindAll()
+    {
+        List<ClinicalDocument> docs = clinicalDocumentDAO.findAll();
+        assertThat(docs.size(), is(2));
+    }
+
+    @Test
+    public void testFind()
+    {
+        ClinicalDocument cd = clinicalDocumentDAO.find(5000);
+        assertThat(cd.getName(), is("Test Patient 1"));
+        assertThat(cd.getXmlContent(), is("<ClinicalDocument>medical data</ClinicalDocument>"));
+    }
+
+    @Test
+    public void testDelete() throws Exception
+    {
+        clinicalDocumentDAO.delete(5000);
+        assertThat(clinicalDocumentDAO.find(5000), is(nullValue()));
+    }
+    
+    @Test
+    public void testSave() throws Exception
+    {
+        LineNumberReader reader = new LineNumberReader(
+                new FileReader("src/test/data/SampleCCDDocument.xml"));
+        StringBuffer buffer = new StringBuffer(70000);
+        String temp = reader.readLine();
+        while (temp != null)
+        {
+            buffer.append(temp);
+            buffer.append('\n');
+            temp = reader.readLine();
+        }
+        
+        reader.close();
+        
+        String xmlDoc = buffer.toString();
+        ClinicalDocument cd = new ClinicalDocument("Test Patient", xmlDoc);
+        clinicalDocumentDAO.save(cd);
+        ClinicalDocument savedCd = clinicalDocumentDAO.find(cd.getId());
+        assertThat(savedCd.getXmlContent(), is(xmlDoc));
+        assertThat(savedCd.getName(), is("Test Patient"));
+    }
+
+}
