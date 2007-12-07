813b4a8e6a570761f5de743947af4a02f690b0bd
Andy Gregorowicz <andrewg@mitre.org>
2007-12-07 14:58:48 +0000

    Switching the location of the test config... I'm seeing some bizarre differences between running the tests in Eclipse vs. Maven

--- models/src/test/java/org/projectlaika/models/dao/ClinicalDocumentDAOTest.java
+++ models/src/test/java/org/projectlaika/models/dao/ClinicalDocumentDAOTest.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-@ContextConfiguration(locations={"spring.xml"})
+@ContextConfiguration(locations={"/spring.xml"})
 public class ClinicalDocumentDAOTest extends AbstractTransactionalJUnit4SpringContextTests
 {
     private ClinicalDocumentDAO clinicalDocumentDAO;
