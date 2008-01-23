3bc483aec75aff9f7b6c8b7b6710ee8885b6c137
Andy Gregorowicz <andrewg@mitre.org>
2008-01-23 20:28:43 +0000

    adding validate_clinical_document_content

--- app/models/vendor_test_plan.rb
+++ app/models/vendor_test_plan.rb
@@ -2,4 +2,19 @@ class VendorTestPlan < ActiveRecord::Base
   has_one :patient_data
   belongs_to :vendor
   has_one :clinical_document
+  
+  def validate_clinical_document_content
+    results = {}
+    document = REXML::Document.new(File.new(clinical_document.full_filename))
+    doc_locations = DocumentLocation.find_all_by_doc_type(clinical_document.doc_type)
+    doc_locations.each do |location|
+      vars = location.bind_variables(patient_data)
+      if vars
+        result = REXML::XPath.match(document, location.xpath_expression, nil, vars)
+        results[location] = result[0]
+      end
+    end
+    
+    results
+  end
 end
