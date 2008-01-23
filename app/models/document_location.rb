f0b575759f7b610b0cdc9b4ccc623c4d868d911d
Andy Gregorowicz <andrewg@mitre.org>
2008-01-23 16:42:41 +0000

    Adding the bind_variables method and reorganizing the specs a bit

--- app/models/document_location.rb
+++ app/models/document_location.rb
@@ -12,4 +12,23 @@ class DocumentLocation < ActiveRecord::Base
 
     vars
   end
+  
+  # Examines provided patient data for values to bind to the variables in
+  # the DocumentLocation's XPath Expression. If all variables can be bound
+  # a hash will be returned containing the variable names as keys. If a
+  # variable cannot be bound, nil will be returned.
+  def bind_variables(patient_data)
+    bound_vars = {}
+    section_object = patient_data.send(section.to_sym)
+    variable_names.each do |variable_name|
+      variable_value = section_object.send(variable_name.to_sym)
+      if variable_value
+        bound_vars[variable_name] = variable_value
+      else
+        return nil
+      end
+    end
+    
+    bound_vars
+  end
 end
--- spec/models/document_location_spec.rb
+++ spec/models/document_location_spec.rb
@@ -1,25 +1,66 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe DocumentLocation do
+describe DocumentLocation, "deals with variables in its XPath expression" do
   before(:each) do
     @document_location = DocumentLocation.new
   end
-
-  it "should be valid" do
-    @document_location.should be_valid
-  end
   
-  it "should find variable names in it's XPath Expression" do
+  it "should return an empty array when there are no variables" do
     @document_location.xpath_expression = '/foo/bar'
     @document_location.variable_names.should be_empty
-    
+  end
+  
+  it "should return a single element array for a single variable" do  
     @document_location.xpath_expression = '/foo/bar/text()=$splat'
     @document_location.variable_names.should have(1).item
     @document_location.variable_names[0].should == 'splat'
-    
+  end
+  
+  it "should find multiple variables and return them in an array" do  
     @document_location.xpath_expression = '/foo/bar/text()=$splat&&/foo/baz/text()=$buzz'
     @document_location.variable_names.should have(2).items
     @document_location.variable_names[0].should == 'splat'
     @document_location.variable_names[1].should == 'buzz'
+  end
+end
+
+describe DocumentLocation, "binds variables" do
+  before(:each) do
+    @document_location = DocumentLocation.new(:section => 'registration_information')
+    @patient_data = PatientData.new
+    @registration_information = RegistrationInformation.new(:first_name => 'Andy',
+                                                            :last_name => 'Gregorowicz')
+    @patient_data.registration_information = @registration_information
+  end
+  
+  it "should bind a single variable properly" do
+    @document_location.xpath_expression = '/foo/bar/text()=$first_name'
+    vars = @document_location.bind_variables(@patient_data)
+    vars.should_not be_empty
+    vars.should have_key("first_name")
+    vars["first_name"].should ==  'Andy'
+  end
+  
+  it "should return an empty hash if there are no variables" do
+    @document_location.xpath_expression = "/foo/bar/text()='Andy'"
+    vars = @document_location.bind_variables(@patient_data)
+    vars.should be_empty
+    vars.should be_a_kind_of(Hash)
+  end
+  
+  it "should return nil when it can't bind a variable" do
+    @document_location.xpath_expression = '/foo/bar/text()=$city'
+    vars = @document_location.bind_variables(@patient_data)
+    vars.should be_nil
+  end
+  
+  it "should bind multiple values properly" do
+    @document_location.xpath_expression = '/foo/first_name/text()=$first_name&&/foo/last_name/text()=$last_name'
+    vars = @document_location.bind_variables(@patient_data)
+    vars.should_not be_empty
+    vars.should have_key("first_name")
+    vars["first_name"].should ==  'Andy'
+    vars.should have_key("last_name")
+    vars["last_name"].should ==  'Gregorowicz'
   end
 end
