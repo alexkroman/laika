37d53edd9e510bf59fbf3d554ad337f01a324a5f
Andy Gregorowicz <andrewg@mitre.org>
2008-01-23 14:59:22 +0000

    adding the variable_names method

--- app/models/document_location.rb
+++ app/models/document_location.rb
@@ -1,3 +1,15 @@
 class DocumentLocation < ActiveRecord::Base
   has_many :namespaces
+  
+  # Finds variables in the XPath Expression for this DocumentLocation
+  # Returns an array containing the variable names. If there are no variables,
+  # an empty array will be returned.
+  def variable_names
+    vars = []
+    xpath_expression.scan(/\$(\w+)/) do |var_name|
+      vars << var_name[0]
+    end
+
+    vars
+  end
 end
