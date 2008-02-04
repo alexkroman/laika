70219d5882122d657cea86bee99d74e028e5fc00
Rob McCready <rmccready@mitre.org>
2008-02-04 22:24:43 +0000

    Re-configured the default action for the root context.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :document_locations
 
-  map.root :controller => "home"
+  map.root :controller => "vendor_test_plans"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
