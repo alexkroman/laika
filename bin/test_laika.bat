f752d3c60a0aca3efbf64f225c7e141870796749
Rob McCready <rmccready@mitre.org>
2008-02-08 21:10:24 +0000

    Command on Windows to run the test script.

---
+++ bin/test_laika.bat
@@ -0,0 +1,24 @@
+REM *******************************************
+REM *** Filename   : start_laika.bat
+REM *** Created by : Rob McCready 
+REM ***            : primes the Laika database 
+REM ***	           : with test data
+REM *******************************************
+
+REM ******************************************* 
+REM Move up one directory so the paths are set
+REM *******************************************
+pushd ..
+
+REM *******************************************
+REM Start up the Healthstories server
+REM *******************************************
+rake spec
+
+REM *******************************************
+REM Once the server terminates, return to the 
+REM orginating 'bin' directory from where this 
+REM script was called
+REM *******************************************
+popd
+
