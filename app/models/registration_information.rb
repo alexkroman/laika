00dbb063d0785fd2086f3053bb33e278b3e81e4f
Andy Gregorowicz <andrewg@mitre.org>
2008-02-08 21:34:05 +0000

    restored name validation

--- app/models/registration_information.rb
+++ app/models/registration_information.rb
@@ -7,12 +7,14 @@ class RegistrationInformation < ActiveRecord::Base
     patient_element = REXML::XPath.first(document, '/cda:ClinicalDocument/cda:recordTarget/cda:patientRole', {'cda' => 'urn:hl7-org:v3'})
     if patient_element
       errors << XmlHelper.match_value(patient_element, 'cda:id/@extension', self.person_identifier)
-      # TODO: Need to deal with more than a single name being present
-      # errors << XpathExpression.match_value(patient_element, 'name_prefix', doc_type, self.person_name.name_prefix)
-      # errors << XpathExpression.match_value(patient_element, 'first_name', doc_type, self.person_name.first_name)
-      # errors << XpathExpression.match_value(patient_element, 'last_name', doc_type, self.person_name.last_name)
-      # errors << XpathExpression.match_value(patient_element, 'name_suffix', doc_type, self.person_name.name_suffix)
-      # errors << XpathExpression.match_value(patient_element, 'patient_gender', doc_type, self.gender)
+      name_element = REXML::XPath.first(patient_element, 
+                                        "cda:patient/cda:name[cda:given='#{self.person_name.first_name}' and cda:family='#{self.person_name.last_name}']",
+                                        {'cda' => 'urn:hl7-org:v3'})
+      errors << XmlHelper.match_value(name_element, 'cda:prefix', self.person_name.name_prefix)
+      errors << XmlHelper.match_value(name_element, 'cda:given', self.person_name.first_name)
+      errors << XmlHelper.match_value(name_element, 'cda:family', self.person_name.last_name)
+      errors << XmlHelper.match_value(name_element, 'cda:suffix', self.person_name.name_suffix)
+      #errors << XpathExpression.match_value(patient_element, 'patient_gender', doc_type, self.gender)
       # # TODO: Need to verfiy birth date somehow... unclear how to do this from the CDA/CCD/C32 specs
       # errors << XpathExpression.match_value(patient_element, 'patient_marital_status', doc_type, self.marital_status)
     else
