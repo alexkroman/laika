14b18ed3f7ebab663f5d768efc988e18dd65c64e
Rob McCready <rmccready@mitre.org>
2008-02-12 15:42:09 +0000

    Fixed the database creation script to work with new rake task.

--- bin/prime_database.bat
+++ bin/prime_database.bat
@@ -13,7 +13,7 @@ pushd ..
 REM *******************************************
 REM Start up the Healthstories server
 REM *******************************************
-rake db:fixtures:load env="development" FIXTURE_DIR="spec/fixtures" --trace
+rake db:fixtures:load_from_dir env="development" FIXTURE_DIR="spec/fixtures" --trace
 
 REM *******************************************
 REM Once the server terminates, return to the 
