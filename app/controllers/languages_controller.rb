47edf98d12e10a194070f937800651f46efea006
Rob McCready <rmccready@mitre.org>
2008-02-13 22:53:04 +0000

    Removed some less-than-great RHTML code for the select drop downs.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,39 +5,34 @@ class LanguagesController < PatientDataChildController
     @languageAbilityModes = LanguageAbilityMode.find(:all, :order => "name ASC")
     @language = Language.new
     render :partial  => 'edit', :locals => {:language => @language,
-                                            :patient_data => @patient_data}
+      :patient_data => @patient_data}
   end
-
+  
   def edit
     @isoLanguages = IsoLanguage.find(:all, :order => "name ASC")
     @isoCountries = IsoCountry.find(:all, :order => "name ASC")
     @languageAbilityModes = LanguageAbilityMode.find(:all, :order => "name ASC")
     @language = @patient_data.languages.find(params[:id])
     render :partial  => 'edit', :locals => {:language => @language,
-                                            :patient_data => @patient_data}
+      :patient_data => @patient_data}
   end
-
+  
   def create
     
-    # find the associated meta-data
-    isoLanguage = IsoLanguage.find(params[:iso_language_id])
-    isoCountry = IsoCountry.find(params[:iso_country_id])
-    languageAbilityMode = LanguageAbilityMode.find(params[:language_ability_mode_id])
-    
-    @language = Language.new(:iso_language => isoLanguage, :iso_country => isoCountry, :language_ability_mode => languageAbilityMode)
+    @language = Language.new(params[:language])
     @patient_data.languages << @language
     render :partial  => 'create', :locals => {:language => @language,
-                                              :patient_data => @patient_data}
+      :patient_data => @patient_data}
   end
-
+  
   def update
     @language = @patient_data.languages.find(params[:id])
     @language.update_attributes(params[:language])
     
     render :partial  => 'show', :locals => {:language => @language,
-                                            :patient_data => @patient_data}
+      :patient_data => @patient_data}
   end
-
+  
   def destroy
     @language = @patient_data.languages.find(params[:id])
     @language.destroy
--- app/models/person_like.rb
+++ app/models/person_like.rb
@@ -4,7 +4,7 @@ module PersonLike
       has_one :person_name, :as => :nameable
       has_one :address, :as => :addressable
       has_one :telecom, :as => :reachable
-
+      
       def copy
         copied_person_like = self.clone
         copied_person_like.save!
@@ -15,88 +15,15 @@ module PersonLike
       end
       
       def update_person_attributes(params)
-        
         self.person_name.update_attributes(params[:person_name])
-        self.address.update_attributes(params[:address])
-        self.address = Address.new(params[:address])
-        
-        if (params[:iso_country_id] != nil)
-          self.address.iso_country = IsoCountry.find(params[:iso_country_id])
-          self.address.save!
-        end
-        
-        if (params[:contact_type_id] != nil)
-          self.contact_type = ContactType.find(params[:contact_type_id])
-        end
-       
-        if (params[:relationship_id] != nil)
-          self.relationship = Relationship.find(params[:relationship_id])
-        end
-        
-        if (params[:marital_status_id] != nil)
-          self.marital_status = MaritalStatus.find(params[:marital_status_id])
-        end
-        
-        if (params[:religion_id] != nil)
-          self.religion = Religion.find(params[:religion_id])
-        end
-        
-        if (params[:gender_id] != nil)
-          self.gender = Gender.find(params[:gender_id])
-        end
-        
-        if (params[:race_id] != nil)
-          self.race = Race.find(params[:race_id])
-        end
-        
-        if (params[:ethnicity_id] != nil)
-          self.ethnicity = Ethnicity.find(params[:ethnicity_id])
-        end
-        self.save!
-
+        self.address.update_attributes(params[:address]) 
         self.telecom.update_attributes(params[:telecom])
       end
       
       def create_person_attributes(params)
         
         self.person_name = PersonName.new(params[:person_name])
-        
         self.address = Address.new(params[:address])
-        if (params[:iso_country_id] != nil)
-          self.address.iso_country = IsoCountry.find(params[:iso_country_id])
-          self.address.save!
-        end
-        
-        if (params[:contact_type_id] != nil)
-          self.contact_type = ContactType.find(params[:contact_type_id])
-        end
-       
-        if (params[:relationship_id] != nil)
-          self.relationship = Relationship.find(params[:relationship_id])
-        end
-        
-        if (params[:marital_status_id] != nil)
-          self.marital_status = MaritalStatus.find(params[:marital_status_id])
-        end
-        
-        if (params[:religion_id] != nil)
-          self.religion = Religion.find(params[:religion_id])
-        end
-        
-        if (params[:gender_id] != nil)
-          self.gender = Gender.find(params[:gender_id])
-        end
-        
-        if (params[:race_id] != nil)
-          self.race = Race.find(params[:race_id])
-        end
-        
-        if (params[:ethnicity_id] != nil)
-          self.ethnicity = Ethnicity.find(params[:ethnicity_id])
-        end
-        
-        self.save!
-   
         self.telecom = Telecom.new(params[:telecom])
       end
     end
