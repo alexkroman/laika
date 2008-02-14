5b75483e85cddc6434a4cf462ed88adfbe56285a
Andy Gregorowicz <andrewg@mitre.org>
2008-02-14 01:57:45 +0000

    Switching to the new validation structure

--- app/models/vendor_test_plan.rb
+++ app/models/vendor_test_plan.rb
@@ -6,9 +6,7 @@ class VendorTestPlan < ActiveRecord::Base
   has_one :clinical_document
   
   def validate_clinical_document_content
-    results = {}
     document = REXML::Document.new(File.new(clinical_document.full_filename))
-    results["registration_information"] = patient_data.registration_information.check_content(document, clinical_document.doc_type)
-    results
+    patient_data.validate_c32(document)
   end
 end
