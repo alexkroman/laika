b926749ce89ab49e9a8ad45eb633e4cfa47f6c41
Andy Gregorowicz <andrewg@mitre.org>
2008-02-15 13:08:21 +0000

    adding specs for helpers, controllers and views

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ApplicationHelper do
+  
+  
+  
+  
+  it "should return a HTTP method of post when a AR model is new" do
+    http_method(PatientData.new).should == 'post'
+  end
+  
+  it "should return a HTTP method of put when a AR model is not new" do
+    mock_model = mock("a_model")
+    mock_model.stub!(:new_record?).and_return(false)
+    http_method(mock_model).should == 'put'
+  end
+  
+end
