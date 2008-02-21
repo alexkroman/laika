f884040c8a419573c7376f16b484e536e831cba8
Andy Gregorowicz <andrewg@mitre.org>
2008-02-21 18:46:22 +0000

    Implementing the input for allergies

--- app/controllers/allergies_controller.rb
+++ app/controllers/allergies_controller.rb
@@ -1,18 +1,23 @@
 class AllergiesController < PatientDataChildController
+  # TODO: Need a way to nil out the end_event through the web ui
+
   def new
     @allergy = Allergy.new
+    @severity_terms = SeverityTerm.find(:all)
+    @adverse_event_types = AdverseEventType.find(:all)
     render :partial  => 'edit', :locals => {:allergy => @allergy,
                                             :patient_data => @patient_data}
   end
 
   def edit
+    @severity_terms = SeverityTerm.find(:all)
+    @adverse_event_types = AdverseEventType.find(:all)
     @allergy = @patient_data.allergies.find(params[:id])
     render :partial  => 'edit', :locals => {:allergy => @allergy,
                                             :patient_data => @patient_data}
   end
   
   def create
-    
     @allergy = Allergy.new(params[:allergy])
     @patient_data.allergies << @allergy
     render :partial  => 'create', :locals => {:allergy => @allergy,
--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,2 +1,5 @@
 class Allergy < ActiveRecord::Base
+  belongs_to :patient_data
+  belongs_to :adverse_event_type
+  belongs_to :severity_term
 end
