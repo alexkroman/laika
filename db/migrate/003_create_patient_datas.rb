77ee9fd165e8d22753ded809c81f9ebf5da4a407
Andy Gregorowicz <andrewg@mitre.org>
2008-02-21 21:16:54 +0000

    starting to enter the allergy test data, and providing the ability to specify no known allergies

--- db/migrate/003_create_patient_datas.rb
+++ db/migrate/003_create_patient_datas.rb
@@ -2,6 +2,7 @@ class CreatePatientDatas < ActiveRecord::Migration
   def self.up
     create_table :patient_data do |t|
       t.string :name
+      t.boolean :no_known_allergies
       t.timestamps
       t.belongs_to :vendor_test_plan
       t.belongs_to :user, :null => false
