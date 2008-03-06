f64cac77951191483c9379b5fa33049469934b77
Andy Gregorowicz <andrewg@mitre.org>
2008-03-06 16:41:46 +0000

    Fixing xpath discrepancy between C32 spec and reality

--- app/models/severity_term.rb
+++ app/models/severity_term.rb
@@ -3,7 +3,7 @@ class SeverityTerm < ActiveRecord::Base
   def validate_c32(severity_element)
     errors = []
     errors << match_value(severity_element, 'cda:text', 'name', self.name)
-    errors << match_value(severity_element, 'cda:value', 'code', self.code)
+    errors << match_value(severity_element, 'cda:value/@code', 'code', self.code)
     errors.compact
   end
   
