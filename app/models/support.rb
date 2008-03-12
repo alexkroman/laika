c5705697db58c6991ade778a122984f1e1e37909
Andy Gregorowicz <andrewg@mitre.org>
2008-03-12 15:56:58 +0000

    adding time validation to support

--- app/models/support.rb
+++ app/models/support.rb
@@ -12,6 +12,20 @@ class Support < ActiveRecord::Base
     support = REXML::XPath.first(xml, "/cda:ClinicalDocument/cda:participant/cda:associatedEntity[cda:associatedPerson/cda:name/text() = $name ] | /cda:ClinicalDocument/cda:recordTarget/cda:patientRole/cda:patient/cda:guardian[cda:guardianPerson/cda:name/text() = $name]",
         {'cda' => 'urn:hl7-org:v3'}, {"name" => name})
     if support
+      time_element = REXML::XPath.first(support, "../cda:time", {'cda' => 'urn:hl7-org:v3'})
+      if time_element
+        if self.start_support
+          errors << match_value(time_element, "cda:low/@value", "start_support", self.start_support.to_formatted_s(:hl7_ts))
+        end
+        if self.end_support
+          errors << match_value(time_element, "cda:high/@value", "end_support", self.end_support.to_formatted_s(:hl7_ts))
+        end
+      else
+        errors <<  ContentError.new(:section => "Support", 
+                            :subsection => "date",
+                            :error_message => "No time element found in the support",
+                            :location => support.xpath)
+      end
       if self.address
         add =  REXML::XPath.first(support,"cda:addr",{'cda' => 'urn:hl7-org:v3'})
         if add
