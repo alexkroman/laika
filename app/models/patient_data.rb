ad7609487bc7ea75a994a17f70f8737d3d2988e4
Rob McCready <rmccready@mitre.org>
2008-03-18 01:00:42 +0000

    Cleaned up and aligned some of Rob's fancy XML generation code

--- app/models/patient_data.rb
+++ app/models/patient_data.rb
@@ -66,71 +66,70 @@ class PatientData < ActiveRecord::Base
   
   
   def to_c32(xml = Builder::XmlMarkup.new)
-       
+      
+    xml.ClinicalDocument("xsi:schemaLocation" => "urn:hl7-org:v3 http://xreg2.nist.gov:8080/hitspValidation/schema/cdar2c32/infrastructure/cda/C32_CDA.xsd", 
+                         "xmlns" => "urn:hl7-org:v3", 
+                         "xmlns:sdct" => "urn:hl7-org:sdct", 
+                         "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance") {
+      xml.typeId("root" => "2.16.840.1.113883.1.3", 
+                 "extension" => "POCD_HD000040")
+      xml.templateId("root" => "2.16.840.1.113883.3.27.1776", 
+                     "assigningAuthorityName" => "CDA/R2")
+      xml.templateId("root" => "2.16.840.1.113883.10.20.1", 
+                     "assigningAuthorityName" => "CCD")
+      xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.1", 
+                     "assigningAuthorityName" => "HITSP/C32")
+      xml.id("root" => "2.16.840.1.113883.3.72", 
+             "extension" => "Laika C32 Test", 
+             "assigningAuthorityName" => "Laika: An Open Source EHR Testing Framework projectlaika.org")
+      xml.code("code" => "34133-9", 
+               "displayName" => "Summarization of patient data", 
+               "codeSystem" => "2.16.840.1.113883.6.1", 
+               "codeSystemName" => "LOINC")
+      xml.title(name)
+      xml.effectiveTime("value" => updated_at.strftime("%Y%m%d%H%M%S-0500"))
+      xml.confidentialityCode
+      xml.languageCode("code" => "en-US")
+      xml.recordTarget {
+        xml.patientRole {
+          xml.id("extension" => "24602", 
+                 "root" => "SomeClinicalOrganizationOID", 
+                 "assignmentAuthorityName" => "Some Clinical Organization Name") 
+            registration_information.andand.to_c32(xml)
+            providers.andand.each do |provider|
+              provider.to_c32(xml)
+            end
+        }   
+      }
+      
+      if support && support.contact_type && support.contact_type.code != "GUARD" 
+        support.to_c32(xml)
+      end          
         
-          xml.ClinicalDocument("xsi:schemaLocation" => "urn:hl7-org:v3 http://xreg2.nist.gov:8080/hitspValidation/schema/cdar2c32/infrastructure/cda/C32_CDA.xsd", 
-                           "xmlns" => "urn:hl7-org:v3", 
-                           "xmlns:sdct" => "urn:hl7-org:sdct", 
-                           "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance") {
-        xml.typeId("root" => "2.16.840.1.113883.1.3", 
-                   "extension" => "POCD_HD000040")
-        xml.templateId("root" => "2.16.840.1.113883.3.27.1776", 
-                       "assigningAuthorityName" => "CDA/R2")
-        xml.templateId("root" => "2.16.840.1.113883.10.20.1", 
-                       "assigningAuthorityName" => "CCD")
-        xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.1", 
-                       "assigningAuthorityName" => "HITSP/C32")
-        xml.id("root" => "2.16.840.1.113883.3.72", 
-               "extension" => "Laika C32 Test", 
-               "assigningAuthorityName" => "Laika: An Open Source EHR Testing Framework projectlaika.org")
-        xml.code("code" => "34133-9", 
-                 "displayName" => "Summarization of patient data", 
-                 "codeSystem" => "2.16.840.1.113883.6.1", 
-                 "codeSystemName" => "LOINC")
-        xml.title(name)
-        xml.effectiveTime("value" => updated_at.strftime("%Y%m%d%H%M%S-0500"))
-        xml.confidentialityCode
-        xml.languageCode("code" => "en-US")
-        xml.recordTarget {
-           xml.patientRole {
-           xml.id("extension" => "24602", 
-                  "root" => "SomeClinicalOrganizationOID", 
-                  "assignmentAuthorityName" => "Some Clinical Organization Name") 
-                   
-                   registration_information.andand.to_c32(xml)
-                   providers.andand.each do |provider|
-                        provider.to_c32(xml)
-                    end
-                  
-             }   
-          } 
-          if support && support.contact_type && support.contact_type.code != "GUARD" 
-                                 support.to_c32(xml)
-          end          
-          information_source.andand.to_c32(xml)    
-                            
-                            xml.component {
-                                
-                                # Start xml.structuredBody
-                                xml.structuredBody {
-                                 
-                                    if (pregnant != nil && pregnant == true) 
-                                            xml.component {
-                                              xml.section {
-                                                xml.title "Results"
-                                                xml.text "Patient is currently pregnant"
-                                                xml.entry {
-                                                  xml.observation ("classCode" => "OBS", "moodCode" => "EVN") {
-                                                    xml.value ("xsi:type" => "CD", "code" => "77386006", "displayName" => "Patient currently pregnant", "codeSystem" => "2.16.840.1.113883.6.96", "codeSystemName" => "SNOMED CT") {} 
-                                                  }
-                                                }
-                                              }               
-                                            }
-                                    end                           
-                                } #end structured body
-                          
-                          } # end component                        
-        }      
-  end
+      information_source.andand.to_c32(xml)    
+        
+      xml.component {
+        xml.structuredBody {
+          if (pregnant != nil && pregnant == true) 
+            xml.component {
+              xml.section {
+                xml.title "Results"
+                xml.text "Patient is currently pregnant"
+                xml.entry {
+                  xml.observation ("classCode" => "OBS", "moodCode" => "EVN") {
+                    xml.value ("xsi:type" => "CD", 
+                               "code" => "77386006", 
+                               "displayName" => "Patient currently pregnant", 
+                               "codeSystem" => "2.16.840.1.113883.6.96", 
+                               "codeSystemName" => "SNOMED CT") {} 
+                  }
+                }
+              }               
+            }
+          end                           
+        }                   
+      }                         
+    }      
+  end  
   
 end
