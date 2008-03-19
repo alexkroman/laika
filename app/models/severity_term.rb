1ad7d34d63da4297eacd20eaf66f02b7e259eac2
Andy Gregorowicz <andrewg@mitre.org>
2008-03-19 17:24:31 +0000

    now derefing the severity term like the spec says moving the allergy test data into its own xml file instead of a shared c32

--- app/models/severity_term.rb
+++ app/models/severity_term.rb
@@ -2,9 +2,24 @@ class SeverityTerm < ActiveRecord::Base
   
   include MatchHelper
 
+  @@default_namespaces = {"cda"=>"urn:hl7-org:v3"}
+
   def validate_c32(severity_element)
     errors = []
-    errors << match_value(severity_element, 'cda:text', 'name', self.name)
+    severity_text = REXML::XPath.first(severity_element,"cda:text",@@default_namespaces)
+    if severity_text
+      derefed_text = deref(severity_text)
+      if derefed_text != self.name
+        errors << ContentError.new(:section => "Allergies", :subsection => "SeverityTerm",
+                                   :error_message => "Severity term #{self.name} does not match #{derefed_text}",
+                                   :location => severity_text.xpath)
+      end
+    else
+      errors << ContentError.new(:section => "Allergies", :subsection => "SeverityTerm",
+                                 :error_message => "Unable to find severity term text",
+                                 :location => severity_element.xpath)
+      
+    end
     errors << match_value(severity_element, 'cda:value/@code', 'code', self.code)
     errors.compact
   end
@@ -17,4 +32,16 @@ class SeverityTerm < ActiveRecord::Base
     'severity'
   end
 
+  # TODO: Pull this code out into a helper module
+  private 
+  def deref(code)
+      if code
+         ref = REXML::XPath.first(code,"cda:reference",@@default_namespaces)
+         if ref
+            REXML::XPath.first(code.document,"//cda:content[@ID=$id]/text()",@@default_namespaces,{"id"=>ref.attributes['value'].gsub("#",'')}) 
+         else
+            nil
+         end
+      end 
+  end
 end
--- spec/models/allergy_spec.rb
+++ spec/models/allergy_spec.rb
@@ -4,7 +4,7 @@ describe Allergy, "it can validate allergy entries in a C32" do
   fixtures :allergies, :severity_terms, :adverse_event_types
   
   it "should verify an allergy matches in a C32 doc" do
-    document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/joe_c32.xml'))
+    document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/allergies/joe_allergy.xml'))
     joe_allergy = allergies(:joes_allergy)
     errors = joe_allergy.validate_c32(document)
     errors.should be_empty
