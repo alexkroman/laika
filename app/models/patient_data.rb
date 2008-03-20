a3e25b4957278b287f064d2498e8565a5dea8ac1
Rob McCready <rmccready@mitre.org>
2008-03-20 23:33:38 +0000

    Conditions needs to be tested.

--- app/models/patient_data.rb
+++ app/models/patient_data.rb
@@ -65,11 +65,11 @@ class PatientData < ActiveRecord::Base
     end 
     
     # Conditions
-    #if self.conditions
-    #  self.conditions.each do |condition|
-    #    errors.concat(condition.validate_c32(clinical_document))
-    #  end  
-    #end
+    if self.conditions
+      self.conditions.each do |condition|
+        errors.concat(condition.validate_c32(clinical_document))
+      end  
+    end
     
     # Information Source
     if self.information_source
