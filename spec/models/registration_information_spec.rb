ef855971b8d5948bd6fd728d74039719e3d39662
Andy Gregorowicz <andrewg@mitre.org>
2008-03-21 00:13:58 +0000

    ensuring that all of the child values for RegistrationInformation get loaded and checked in the specs

--- spec/models/registration_information_spec.rb
+++ spec/models/registration_information_spec.rb
@@ -2,20 +2,20 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RegistrationInformation, "can vaildate it's content" do
   fixtures :patient_data, :registration_information, :person_names, :addresses,
-           :telecoms, :genders
+           :telecoms, :genders, :marital_statuses, :ethnicities, :races, :religions
   
   it "should verify a person id matches in a C32 doc" do
     document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/joe_c32.xml'))
     joe_reg = registration_information(:joe_smith)
     errors = joe_reg.validate_c32(document)
-    puts errors
+    puts errors.map {|e| e.error_message }.join(' ')
     errors.should be_empty
   end
 end
 
 describe RegistrationInformation, "can create a C32 representation of itself" do
   fixtures :patient_data, :registration_information, :person_names, :addresses,
-           :telecoms, :genders
+           :telecoms, :genders, :marital_statuses, :ethnicities, :races, :religions
   
   it "should create valid C32 content" do
     joe_reg = registration_information(:joe_smith)
