04890d70614c8e00e19ca3e4591252f15c85778d
Rob McCready <rmccready@mitre.org>
2008-03-31 17:12:15 +0000

    Since we can't figure out how to handle tagging, I am performing delete and copy to the branches for production and stage.

--- bin/tag_laika.bat
+++ bin/tag_laika.bat
@@ -1,2 +1,4 @@
-REM svn switch https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/production
-REM svn merge -r 630:637 https://laika.svn.sourceforge.net/svnroot/laika/webapp/trunk
+REM svn delete https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/production -m "Recreating the Laika tags"
+REM svn copy https://laika.svn.sourceforge.net/svnroot/laika/webapp/trunk https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/production -m "Recreating the production tag"
+REM svn delete https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/stage -m "Recreating the Laika tags"
+REM svn copy https://laika.svn.sourceforge.net/svnroot/laika/webapp/trunk https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/stage -m "Recreating the stage tag"
