3fc85255bae202af366ee4275691055ee06f0f9a
Rob McCready <rmccready@mitre.org>
2008-04-01 20:32:16 +0000

    Fixed an error in the migration script when the database is rebuilt

--- db/migrate/058_remove_not_applicable_language_mode.rb
+++ db/migrate/058_remove_not_applicable_language_mode.rb
@@ -1,8 +1,15 @@
 class RemoveNotApplicableLanguageMode < ActiveRecord::Migration
   
   def self.up
-    notApplicable = LanguageAbilityMode.find_by_name('n/a')
-    notApplicable.destroy
+    # will attempt to find LanguageAbilityMode with name 'n/a' 
+    # and destroy it.  
+    # This would have been created from the migration specs
+    begin 
+      notApplicable = LanguageAbilityMode.find_by_name('n/a')
+      notApplicable.destroy
+    # don't do anything if this fails
+    rescue
+    end
   end
 
   def self.down
