89a4fb1afe4998708a39c10573c52e8d4c69ff1f
Rob McCready <rmccready@mitre.org>
2008-07-14 22:53:32 +0000

    support for separating the XML validator errors with content inspection warnings

--- lib/validation_util.rb
+++ lib/validation_util.rb
@@ -1,13 +1,14 @@
-#
-#Real simple utils to access the external schematron validation
+# Real simple utility to access the external schematron validation web service
 class ValidationUtil
+  
    require 'net/http'
    require 'rexml/document'
+   
    def ValidationUtil.validate(docType, xml)
-   puts VALIDATION_URL
-       res = Net::HTTP.post_form(URI.parse(VALIDATION_URL),
-           {'docType'=>docType, 'xmlContent'=>xml})     
-           res.value
-        return  REXML::Document.new(res.body)   
+     puts VALIDATION_URL
+     res = Net::HTTP.post_form(URI.parse(VALIDATION_URL), {'docType'=>docType, 'xmlContent'=>xml})     
+     res.value
+     return REXML::Document.new(res.body)   
    end
+   
 end
