308da4abbdfaa927a6d9bf0d84190b21991382f6
Rob McCready <rmccready@mitre.org>
2008-07-28 19:00:27 +0000

    shell script to tag the Laika code from OSX/LINUX

--- bin/tag_laika.sh
+++ bin/tag_laika.sh
@@ -4,3 +4,4 @@ svn delete https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/productio
 svn copy https://laika.svn.sourceforge.net/svnroot/laika/webapp/trunk https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/production -m "Recreating the production tag"
 svn delete https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/stage -m "Recreating the Laika tags"
 svn copy https://laika.svn.sourceforge.net/svnroot/laika/webapp/trunk https://laika.svn.sourceforge.net/svnroot/laika/webapp/tags/stage -m "Recreating the stage tag"
+cd bin
