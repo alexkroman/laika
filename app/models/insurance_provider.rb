a5cd0de93c6e9e5081e3e66782990c076a3dc6a1
Rob McCready <rmccready@mitre.org>
2008-08-07 21:51:29 +0000

    Cleaned up the display of immunization HTML for show and edit

--- app/models/insurance_provider.rb
+++ app/models/insurance_provider.rb
@@ -1,5 +1,5 @@
 class InsuranceProvider < ActiveRecord::Base
-  
+
   strip_attributes!
 
   belongs_to :patient_data
@@ -10,41 +10,41 @@ class InsuranceProvider < ActiveRecord::Base
   has_one    :insurance_provider_patient
   has_one    :insurance_provider_subscriber
   has_one    :insurance_provider_guarantor
-  
+
   include MatchHelper
-  
+
   @@default_namespaces = {"cda"=>"urn:hl7-org:v3"}
-  
+
   #Reimplementing from MatchHelper
   def section_name
     "Insurance Providers Module"
   end
-  
+
   def validate_c32(document)
     errors = []
-    
+
     begin
       section = REXML::XPath.first(document,"//cda:section[cda:templateId/@root='2.16.840.1.113883.10.20.1.9']",@@default_namespaces)
       parentAct = REXML::XPath.first(section,"cda:entry/cda:act[cda:templateId/@root='2.16.840.1.113883.10.20.1.20']",@@default_namespaces)
       childAct = REXML::XPath.first(parentAct,"cda:entryRelationship/cda:act[cda:templateId/@root='2.16.840.1.113883.10.20.1.26']",@@default_namespaces)
-    
+
       if group_number
         errors << match_value(childAct, "cda:id/@root", "group_number", self.group_number.to_s)
       end
-      
+
       if insurance_type 
         code = REXML::XPath.first(childAct,"cda:code[@codeSystem='2.16.840.1.113883.6.255.1336']",@@default_namespaces)
         errors.concat insurance_type.validate_c32(code)
       end
-    
+
       if represented_organization 
         representedOrganization = REXML::XPath.first(childAct,
           "cda:performer[@typeCode='PRF']/cda:assignedEntity[@classCode='ASSIGNED']/cda:representedOrganization[@classCode='ORG']",@@default_namespaces)
         errors << match_value(representedOrganization, "cda:name", "represented_organization_name", self.represented_organization.to_s)
       end
-      
+
       if self.insurance_provider_guarantor && insurance_provider_guarantor.has_any_data
-        
+
         # insurance provider's represented organization test
         if represented_organization
           begin   
@@ -55,12 +55,12 @@ class InsuranceProvider < ActiveRecord::Base
               :error_message => 'Failed checking that the XML element''performer'' has attribute ''typeCode'' that is equal to ''PRF''',
               :type=>'error',
               :location => childAct.xpath)
-          end    
+          end
         end
-        
+
         # TODO start insurance provider's effective date test
         # end insurance provider's effective date test
-        
+
         # insurance provider guarantor
         if insurance_provider_guarantor.person_name.first_name && insurance_provider_guarantor.person_name.last_name
           guarantor_name_element = REXML::XPath.first(childAct, 
@@ -76,12 +76,12 @@ class InsuranceProvider < ActiveRecord::Base
                                        :location => guarantor_name_element.xpath)
           end
         end
-        
+
         # insurance provider subscriber
         #if insurance_provider_subscriber
         #  self.insurance_provider_subscriber.validate_c32(childAct)
         #end
-       
+
       end
 
     rescue
@@ -92,17 +92,17 @@ class InsuranceProvider < ActiveRecord::Base
     end
     errors.compact
   end
-  
+
   def to_c32(xml)
-    xml.entry {
-      xml.act("classCode" => "ACT", "moodCode" => "DEF") {
+    xml.entry do
+      xml.act("classCode" => "ACT", "moodCode" => "DEF") do
         xml.templateId("root" => "2.16.840.1.113883.10.20.1.20", "assigningAuthorityName" => "CCD")
         xml.id("root" => group_number, "extension" => "GroupOrContract#")
         xml.code('code'=>'48768-6', 'displayName'=>'Payment Sources',
             'codeSystem'=>'2.16.840.1.113883.6.1' ,'codeSystemName'=>'LOINC')
         xml.statusCode('code'=>'completed')
-        xml.entryRelationship("typeCode" => "COMP") {
-          xml.act("classCode" => "ACT", "moodCode" => "EVN") {
+        xml.entryRelationship("typeCode" => "COMP") do
+          xml.act("classCode" => "ACT", "moodCode" => "EVN") do
             xml.templateId("root" => "2.16.840.1.113883.10.20.1.26")
             xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.5")
             xml.id("root" => group_number, "extension" => "GroupOrContract#")
@@ -115,28 +115,28 @@ class InsuranceProvider < ActiveRecord::Base
               xml.code("nullFlavour"=>"NA")
             end
             xml.statusCode('code'=>'completed')
-            
+
             # represented organization
             if represented_organization 
-              xml.performer("typeCode" => "PRF") {
-                xml.assignedEntity("classCode" => "ASSIGNED") {
+              xml.performer("typeCode" => "PRF") do
+                xml.assignedEntity("classCode" => "ASSIGNED") do
                   xml.id('root'=>'2.16.840.1.113883.3.88.3.1')
-                  xml.representedOrganization("classCode" => "ORG") {
+                  xml.representedOrganization("classCode" => "ORG") do
                     xml.id("root" => "2.16.840.1.113883.19.5")
                     xml.name represented_organization
-                  }
-                }
-              }
+                  end
+                end
+              end
             end
-            
+
             # guarantor is provided only if there is some non-nil, non-empty data
             if insurance_provider_guarantor && insurance_provider_guarantor.has_any_data
               attrs = (represented_organization) ? {} : {"typeCode" => "PRF"}
-              xml.performer(attrs) {
+              xml.performer(attrs) do
                 if !insurance_provider_guarantor.effective_date.blank?
                   xml.time("value" => insurance_provider_guarantor.effective_date.strftime("%Y%m%d"))
                 end
-                xml.assignedEntity {
+                xml.assignedEntity do
                   xml.id
                   code_atts = {"code"=>"PAYOR","codeSystem" => "2.16.840.1.113883.5.110"}
                   if insurance_type && insurance_type.code == 'PP'
@@ -147,39 +147,39 @@ class InsuranceProvider < ActiveRecord::Base
                       end
                   end
                   xml.code(code_atts)
-                  xml.assignedPerson {
+                  xml.assignedPerson do
                     insurance_provider_guarantor.person_name.to_c32(xml)
-                  }
-                }
-              }
+                  end
+                end
+              end
             end
-            
+
             # patient data is provided only if there is some non-nil, non-empty data
             if insurance_provider_patient && insurance_provider_patient.has_any_data
-              xml.participant("typeCode" => "COV") {
-                xml.participantRole("classCode" => "PAT") {
+              xml.participant("typeCode" => "COV") do
+                xml.participantRole("classCode" => "PAT") do
                   if coverage_role_type
                     xml.code("code" => coverage_role_type.code, 
                              "displayName" => coverage_role_type.name, 
                              "codeSystem" => "2.16.840.1.113883.5.111", 
                              "codeSystemName" => "RoleCode") 
                   end
-                  xml.playingEntity {
+                  xml.playingEntity do
                     insurance_provider_patient.person_name.andand.to_c32(xml)
                     if !insurance_provider_patient.date_of_birth.blank?
                       xml.sdtc(:birthTime, "value" => insurance_provider_patient.date_of_birth.strftime("%Y%m%d"))
                     end
-                  }
-                }
-              }
+                  end
+                end
+              end
             end
-            
+
             insurance_provider_subscriber.andand.to_c32(xml)
 
-          }
-        }
-      }
-    }
+          end
+        end
+      end
+    end
   end
  
  
