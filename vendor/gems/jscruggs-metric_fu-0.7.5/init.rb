ed6a6199ea9139a5865e4228daae4bdee877842c
Andy Gregorowicz <andrewg@mitre.org>
2008-09-16 21:14:12 +0000

    trying to fix load path issues

--- vendor/gems/jscruggs-metric_fu-0.7.5/init.rb
+++ vendor/gems/jscruggs-metric_fu-0.7.5/init.rb
@@ -1,5 +1,4 @@
-
-require_options = ["jscruggs-metric_fu", "metric_fu"]
+require_options = ["metric_fu"]
 if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
   require File.join(File.dirname(__FILE__), 'lib', require_lib)
 else
