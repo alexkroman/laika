a45c5e4a2944fa0b856e73decaec4a9ed88d6bb5
Rob Dingwell <bobd@mitre.org>
2008-12-29 23:15:05 +0000

    adding some comments to make things a bit clearer as to how the validation works

--- lib/validators/umls_validator.rb
+++ lib/validators/umls_validator.rb
@@ -51,25 +51,33 @@ module Validators
          @mapping = YAML.load_file(UMLS_MAPPING_DEFAULT)    
         end   
 
+
+        # Validate a given xml document against the umls db. This validation only looks to see 
+        # if the code 
         def validate(document)
        
          errors = []
          
+         # get every element with a code system attribute - if REXML supported XPath correctly 
+         # we could get every element with a code and codeSystem attribute like so 
+         #     document.elements.to_a("//*[@codeSystem and @code]")
+         #
+         # but REXML doesnt return anything for that , evn though it should, Anyway, for every 
+         # element with a codeSystem and code attribute we look to see if the code is in the code system according to 
+         # the UMLS database.  
          document.elements.to_a("//*[@codeSystem]").each do |el|
            oid = el.attributes["codeSystem"]
            code = el.attributes["code"] 
            name = el.attributes["name"]
            map = @mapping[oid]        
-            
+           # if there isnt a mapping for leave it alone , we know nothing about the code system and cannot pass judgment 
+           # on whether or not it's valid.  
            if map && code 
               cs = map["codesystem"]
               parent = map["umlscode"]
-              valid = true
-              if parent
-                 valid = in_code_system_with_parent(cs,parent,code) 
-              else
-                # valid = in_code_system(cs,code)
-              end              
+              valid = parent ? in_code_system_with_parent(cs,parent,code)  :  in_code_system(cs,code)
+             
+             # figure out how to handle the errors here
               unless valid
                  errors << el
               end          
@@ -87,6 +95,10 @@ module Validators
             if(code_system.nil?)
             else
             sab = code_system["codesystem"]
+            # potential umls parent code-  this is very prevelent in the HL7 
+            # code system where the code system for all of it's entries is HL7V3.0 or similar
+            # but there is a hierarchy mapping that needs to be consulted to determin if the code 
+            # exists in the 'oid' code system
             concept = code_system["umlscode"]
             if concept.nil?    
               return UmlsCodeSystem.find_by_RSAB(sab)
@@ -96,16 +108,24 @@ module Validators
             end
         end
         
-      
+       # if there wasnt a umlscode in the mapping file for the oid in question we do a straight lookup based on
+       # code_system and code and possibly an optional name
         def in_code_system( code_system,  code,  name = nil)
              params = {:sab=>code_system, :code=>code}
-             params[:name ] = name if name            
+             params[:str ] = name if name            
              UmlsConcept.count(:conditions=>params) > 0 
         end
         
+        
+        # If there was a umlscode in the mapping file for the oid then we need to consult the UMLS hierarchy to see
+        # if the given code is in the hierarchy of the parent_code (umlscode).  To do this we obtain all of the concept
+        # entries for the parent_code in the given code_system , there may be more then one.   Then we look in the hierarchy
+        # for all of those entries.   For each hierarchy entry we look to see if the list of potential child codes ( yes there may be more then one entry)
+        # exist in the hierarchy under the parent code.  The hierarchy is determined by a concatination of the concept AUI field entries separated by
+        # '.' .  We use simple LIKE expressions to see of the hierarchy may contain any of the children
         def in_code_system_with_parent( code_system,  parent_code,  code, name = nil)
             params = {:sab=>code_system, :code=>code}
-            params[:name ] = name if name
+            params[:str ] = name if name
             parent_auis = UmlsConcept.find(:all,:conditions=>["sab = ? and code = ?",code_system,parent_code]).collect{|x| x.AUI }
             child_auis =  UmlsConcept.find(:all,:conditions=>params).collect{|x| x.AUI }
 
