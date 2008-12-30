2ed89d47bc729c4936b03fdde20d64fa2f50815c
Rob Dingwell <bobd@mitre.org>
2008-12-30 00:09:40 +0000

    Stubbing out schematron validation

---
+++ lib/validators/schematron_validator.rb
@@ -0,0 +1,20 @@
+module Validators
+  module Schematron
+    class UncompiledValidator
+      
+      def validate(document)
+        
+      end
+      
+    end
+    
+    class CompiledValidator
+      
+      def validate(document)
+        
+      end
+      
+    end
+    
+  end
+end
