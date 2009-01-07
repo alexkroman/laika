7780a6a4944d7c5bf7490437507ef7a6d75608ff
Zack Hobson <zack@opensourcery.com>
2009-01-07 21:37:08 +0000

    PregnancyController cleanup and view spec.
     * added a spec for views/pregnanct/edit.html.erb
     * cleaned up PregnanyController and made it a subclass
        of PatientDataChildController

--- app/controllers/pregnancy_controller.rb
+++ app/controllers/pregnancy_controller.rb
@@ -1,27 +1,16 @@
-class PregnancyController < ApplicationController
+class PregnancyController < PatientDataChildController
 
   def edit
-    @patient_data = PatientData.find params[:patient_data_instance_id]
     render :layout => false
   end
 
   def update
-    @patient_data_id = params[:patient_data_instance_id]
-    @patient_data = PatientData.find(@patient_data_id)
-    pregnancy = false;
-    if params[:pregnant] == 'on'
-      pregnancy = true
-    end
-    @patient_data.pregnant = pregnancy
+    @patient_data.pregnant = (params[:pregnant] == 'on')
     @patient_data.save!
     render :partial  => 'show'
   end
   
-  # DELETE /pregnancy/1
-  # DELETE /pregnancy/1.xml
   def destroy
-    @patient_data_id = params[:patient_data_instance_id]
-    @patient_data = PatientData.find(@patient_data_id)
     @patient_data.pregnant = nil
     @patient_data.save!
     render :partial  => 'show'
---
+++ spec/views/pregnancy/edit.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "pregnancy/edit.html.erb" do
+  it "should show the pregnancy form and checkbox" do
+    assigns[:patient_data] = pd = stub_model(PatientData)
+
+    render "pregnancy/edit.html.erb"
+
+    response.should have_tag("form[action=/pregnancy/update?patient_data_instance_id=#{pd.id}]") do
+      with_tag 'input[type=checkbox][name=pregnant]'
+    end
+  end
+
+  it "should show the pregnancy form and checkbox checked" do
+    assigns[:patient_data] = pd = stub_model(PatientData, :pregnant => true)
+
+    render "pregnancy/edit.html.erb"
+
+    response.should have_tag("form[action=/pregnancy/update?patient_data_instance_id=#{pd.id}]") do
+      with_tag 'input[type=checkbox][name=pregnant][checked=true]'
+    end
+  end
+end
