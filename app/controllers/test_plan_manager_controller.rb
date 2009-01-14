565f418074e0d9bb41316ac21a0ebd08cdfc7d37
Zack Hobson <zack@opensourcery.com>
2009-01-14 21:05:15 +0000

    Fix and re-enable Export Report from the Dashboard. Closes SF ticket 2053633.
     * TestPlanManagerController:
       - modified export to output directly instead of using a static file
     * views/vendor_test_plans/index:
       - restored the "Export Report" link to test_plan_manager/export (it
         was removed in r933)

--- app/controllers/test_plan_manager_controller.rb
+++ app/controllers/test_plan_manager_controller.rb
@@ -49,62 +49,61 @@ class TestPlanManagerController < ApplicationController
     @vendors = Vendor.find(:all)
     @kinds = Kind.find(:all)
 
-    File.open("#{RAILS_ROOT}/public/report.csv", "w") do |f|
-      for user in @users
-        flag1 = 0
-        for vendor in @vendors
-          flag2 = 0
-          for vendor_test_plan in @vendor_test_plans
-            if vendor_test_plan.user_id == user.id	  				
-              if vendor_test_plan.vendor_id == vendor.id
-                if flag1 == 0
-                  f.write user.first_name + " " + user.last_name + "'s Inspections:\n"
-                  flag1 = 1
-                end	
-                if flag2 == 0
-                  f.write "Vendor ID:,"
-                  f.write vendor.public_id + "\n\n"	
-                  flag2 = 1
-                end	
-                f.write "Patient Name:,"
-                f.write vendor_test_plan.patient_data.name + "\n" 	
-                for kind in @kinds
-                  if kind.id == vendor_test_plan.kind_id
-                    f.write "Inspection Type:,"
-                    f.write kind.name + "\n"
-                    break
-                  end
+    report = ""
+
+    @users.each do |user|
+      flag1 = 0
+      @vendors.each do |vendor|
+        flag2 = 0
+        @vendor_test_plans.each do |vendor_test_plan|
+          if vendor_test_plan.user_id == user.id	  				
+            if vendor_test_plan.vendor_id == vendor.id
+              if flag1 == 0
+                report << user.first_name + " " + user.last_name + "'s Inspections:\n"
+                flag1 = 1
+              end	
+              if flag2 == 0
+                report << "Vendor ID:,"
+                report << vendor.public_id + "\n\n"	
+                flag2 = 1
+              end	
+              report << "Patient Name:,"
+              report << vendor_test_plan.patient_data.name + "\n" 	
+              @kinds.each do |kind|
+                if kind.id == vendor_test_plan.kind_id
+                  report << "Inspection Type:,"
+                  report << kind.name + "\n"
+                  break
                 end
-                f.write "Result:,"
-                error_count = 0
-                if vendor_test_plan.validated?
-                  error_count = REXML::XPath.first(vendor_test_plan.clinical_document.validation_report(:xml),"count(//error)")
-                  error_count += vendor_test_plan.content_errors.length	        	          
-                  if error_count > 0
-                    f.write "failure\n"
-                    f.write "Number of errors:, "
-                    f.write error_count 
-                    f.write "\n"
-                  else
-                    f.write "success\n"
-                    f.write "Number of errors:, --\n"
-                  end
-                else
-                  f.write "in progress\n"
-                  f.write "Number of errors:, --\n"		  				
-                end
-                f.write "Last Modified:,"
-                f.write vendor_test_plan.updated_at.strftime("%d.%b.%Y")
-                f.write "\n\n"
               end
+              report << "Result:,"
+              error_count = 0
+              if vendor_test_plan.validated?
+                error_count = REXML::XPath.first(vendor_test_plan.clinical_document.validation_report(:xml),"count(//error)")
+                error_count += vendor_test_plan.content_errors.length	        	          
+                if error_count > 0
+                  report << "failure\n"
+                  report << "Number of errors:, "
+                  report << error_count 
+                  report << "\n"
+                else
+                  report << "success\n"
+                  report << "Number of errors:, --\n"
+                end
+              else
+                report << "in progress\n"
+                report << "Number of errors:, --\n"		  				
+              end
+              report << "Last Modified:,"
+              report << vendor_test_plan.updated_at.strftime("%d.%b.%Y")
+              report << "\n\n"
             end
           end
         end
       end
     end
 
-    redirect_to relative_url_root + "/report.csv"
-
+    send_data report, :filename => "report.csv", :type => 'application/x-download'
   end
 
 end
