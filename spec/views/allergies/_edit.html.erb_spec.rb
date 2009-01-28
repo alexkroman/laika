ecc8c693b5ea133b678b22fd0264587cb001bdfa
Zack Hobson <zack@opensourcery.com>
2009-01-28 19:09:33 +0000

    Added pending specs for SF ticket 2263302.

--- spec/views/allergies/_edit.html.erb_spec.rb
+++ spec/views/allergies/_edit.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
+describe "all allergy forms", :shared => true do
+  it "should include severity information" do
+    pending "SF ticket 2263302"
+    render :partial  => 'allergies/edit', :locals => {:allergy => @allergy,
+                                                       :patient_data => @patient_data}
+    response.should have_tag("form") do
+      with_tag "select[id=allergy_severity_term_id]"
+    end
+  end
+end
+
 describe "allergies/_edit.html.erb" do
   fixtures :users
 
@@ -8,6 +19,7 @@ describe "allergies/_edit.html.erb" do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @allergy = Allergy.create!(:patient_data => @patient_data)
     end
+    it_should_behave_like "all allergy forms"
 
     it "should render the edit form with method PUT" do
       render :partial  => 'allergies/edit', :locals => {:allergy => @allergy,
@@ -16,6 +28,7 @@ describe "allergies/_edit.html.erb" do
         with_tag "input[name=_method][value=put]"
       end
     end
+
   end
 
   describe "without an existing allergy (allergies/new)" do
@@ -23,6 +36,7 @@ describe "allergies/_edit.html.erb" do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @allergy = Allergy.new
     end
+    it_should_behave_like "all allergy forms"
 
     it "should render the edit form with method POST" do
       render :partial  => 'allergies/edit', :locals => {:allergy => @allergy,
