407964803b1f6f9db0cbd993824b8e2b06d8c444
Zack Hobson <zack@opensourcery.com>
2009-01-28 18:36:38 +0000

    Fixed LAIKA_REVISION initializer.

--- config/initializers/laika_globals.rb
+++ config/initializers/laika_globals.rb
@@ -5,19 +5,18 @@ ERROR_EMAIL = "rmccready@mitre.org"
 
 # Extract the subversion revision number from the
 # Capistrano REVISION file or the .svn/entries file
-begin
+LAIKA_REVISION = begin
   revision_path = File.dirname(__FILE__) + '/../../REVISION'
   entries_path = '.svn/entries'
   if File.exists?(revision_path)
     File.open(revision_path, "r") do |rev|
-      LAIKA_REVISION = rev.readline.chomp
+      rev.readline.chomp
     end
   elsif File.exists?(entries_path)
-    File.open(entries_path, "r").enum_with_index do |line,i|
-      if i == 3
-        LAIKA_REVISION = line
-        break
-      end
+    File.open(entries_path, "r") do |entries|
+      entries.lines.take(4).last.chomp
     end
+  else
+    'x'
   end
 end
