26dc1499ce565e761b984fd0bc1440e81633d40f
Zack Hobson <zack@opensourcery.com>
2009-01-28 19:57:28 +0000

    Added a pending spec for SF ticket 2119631.

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,41 +1,44 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Provider, "can validate itself" do
-  fixtures :providers,:provider_roles,:provider_types, :person_names
+describe Provider do
+  fixtures :providers,:provider_roles,:provider_types, :person_names, :telecoms, :addresses
   
   before(:each) do
     @provider = providers(:rn_mary_smith)
   end  
+
+  it "should require start/end service" do
+    pending "SF ticket 2119631"
+    @provider.update_attributes!(:start_service => nil, :end_service => nil)
+    document = document_for(@provider)
+    @provider.validate_c32(document).should_not be_empty
+  end                   
   
-  it "should validate without errors" do
-      document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/jenny_healthcare_provider.xml'))
-      errors = @provider.validate_c32(document.root)
-      #puts errors.map { |e| e.error_message }.join(' ')
-      errors.should be_empty
-      
+  it "should validate itself without errors" do
+    document = REXML::Document.new(File.new(RAILS_ROOT + '/spec/test_data/jenny_healthcare_provider.xml'))
+    @provider.validate_c32(document).should be_empty
   end 
   
-end
-
-describe Provider, "can create a C32 representation of itself" do
-  fixtures :providers,:provider_roles,:provider_types, :person_names, :telecoms, :addresses
-  
   it "should create valid C32 content" do
-    provider = providers(:rn_mary_smith)
-    document = LaikaSpecHelper.build_c32 do |xml|
-        xml.documentationOf do 
-            xml.serviceEvent("classCode" => "PCPR") do 
-              xml.effectiveTime  do
-                 xml.low('value'=> "0")
-                 xml.high('value'=> "2010")
-              end     
-            provider.to_c32(xml)
-            end 
-        end
-
-    end
-
-    errors = provider.validate_c32(document.root)
-    errors.should be_empty
+    document = document_for(@provider)
+    @provider.validate_c32(document).should be_empty
   end
+
+  #
+  # helpers
+  #
+  def document_for(provider)
+    LaikaSpecHelper.build_c32 do |xml|
+      xml.documentationOf do 
+        xml.serviceEvent("classCode" => "PCPR") do 
+          xml.effectiveTime  do
+            xml.low('value'=> "0")
+            xml.high('value'=> "2010")
+          end     
+        provider.to_c32(xml)
+        end 
+      end
+    end
+  end
+
 end
