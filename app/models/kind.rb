b3dd18f30ff98e3f9dd684f1a9b2781ffd8fc0ab
Zack Hobson <zack@opensourcery.com>
2009-02-12 01:09:27 +0000

    Added test_type to Kind, labelled C32 tests.
    Closes SF ticket #2539090
    
     * new migration 20090212003045 adds test_type to kinds
       - also sets test_type = 'C32' for existing kinds
     * models/kind.rb
       - added display_name
       - tweaked select_options to use test_type
     * views/vendor_test_plans/index.html.erb:
       - use kind.display_name
     * spec/fixtures/kinds.yml:
       - add test_type: C32 to existing kinds

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,4 +1,7 @@
 class Kind < ActiveRecord::Base
-  has_select_options
+  has_select_options(:order => 'test_type ASC, name ASC') {|r| [r.display_name, r.id] }
 
+  def display_name
+    "#{test_type} #{name}"
+  end
 end
