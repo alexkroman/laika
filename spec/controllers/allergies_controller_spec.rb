d68e0f239c91e68df857e06276e4aa1c470fb042
Zack Hobson <zack@opensourcery.com>
2009-04-08 11:55:21 -0700

    Cleanup and spec for AllergiesController.
     * app/views/allergies/_create.html.erb
       - renamed to app/views/allergies/create.html.erb
     * app/views/allergies/_edit.html.erb
       - renamed to app/views/allergies/edit.html.erb
       - also renamed associated view spec
     * app/controllers/allergies_controller.rb
       - disable layout
       - render edit and create as templates
       - do not needlessly set instance variables
     * app/views/patient_data/show.html.erb b/app/views/patient_data/show.html.erb
       - removed a typo
     * spec/controllers/allergies_controller_spec.rb
       - added a spec suite for AllergiesController

--- spec/controllers/allergies_controller_spec.rb
+++ spec/controllers/allergies_controller_spec.rb
@@ -1,5 +1,77 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AllergiesController do
+  fixtures :patient_data, :allergies
+
+  before do
+    @user = stub(:user)
+    controller.stub!(:current_user).and_return(@user)
+    @patient_data = patient_data(:joe_smith)
+  end
+
+  it "should render edit template on get new" do
+    get :new, :patient_data_instance_id => @patient_data.id.to_s
+    response.should render_template('allergies/edit')
+  end
+
+  it "should assign @allergy on get new" do
+    get :new, :patient_data_instance_id => @patient_data.id.to_s
+    assigns[:allergy].should be_new_record
+  end
+
+  it "should render edit template on get edit" do
+    get :edit, :patient_data_instance_id => @patient_data.id.to_s, :id => @patient_data.allergies.first.id.to_s
+    response.should render_template('allergies/edit')
+  end
+
+  it "should assign @allergy on get edit" do
+    get :edit, :patient_data_instance_id => @patient_data.id.to_s, :id => @patient_data.allergies.first.id.to_s
+    assigns[:allergy].should == @patient_data.allergies.first
+  end
+
+  it "should render create template on post create" do
+    post :create, :patient_data_instance_id => @patient_data.id.to_s
+    response.should render_template('allergies/create')
+  end
+
+  it "should assign @allergy on post create" do
+    post :create, :patient_data_instance_id => @patient_data.id.to_s
+    assigns[:allergy].should_not be_new_record
+  end
+
+  it "should add an allergy on post create" do
+    old_allergy_count = @patient_data.allergies.count
+    post :create, :patient_data_instance_id => @patient_data.id.to_s
+    @patient_data.allergies(true).count.should == old_allergy_count + 1
+  end
+
+  it "should render show partial on put update" do
+    put :update, :patient_data_instance_id => @patient_data.id.to_s, :id => @patient_data.allergies.first.id.to_s
+    response.should render_template('allergies/_show')
+  end
+
+  it "should assign @allergy on put update" do
+    existing_allergy = @patient_data.allergies.first
+    put :update, :patient_data_instance_id => @patient_data.id.to_s, :id => existing_allergy.id.to_s
+    assigns[:allergy].should_not be_new_record
+    assigns[:allergy].should == existing_allergy
+  end
+
+  it "should render no_known_allergies_link partial on delete destroy" do
+    delete :destroy, :patient_data_instance_id => @patient_data.id.to_s, :id => @patient_data.allergies.first.id.to_s
+    response.should render_template('allergies/_no_known_allergies_link')
+  end
+
+  it "should not assign @allergy on delete destroy" do
+    delete :destroy, :patient_data_instance_id => @patient_data.id.to_s, :id => @patient_data.allergies.first.id.to_s
+    assigns[:allergy].should be_nil
+  end
+
+  it "should remove from @patient_data.allergies on delete destroy" do
+    existing_allergy = @patient_data.allergies.first
+    delete :destroy, :patient_data_instance_id => @patient_data.id.to_s, :id => existing_allergy.id.to_s
+    @patient_data.allergies(true).should_not include(existing_allergy)
+  end
 
 end
+
