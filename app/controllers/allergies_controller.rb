35fb9b62976e42c7326c3b4dd7197eb728f53c8f
Zack Hobson <zack@opensourcery.com>
2009-04-09 11:52:01 -0700

    CRUD operations in PatientDataChildController, SF ticket #2748115.
     * app/controllers/patient_data_child_controller.rb
       - implement generalized new, edit, create, update, destroy
     * app/controllers/advance_directives_controller.rb
       - remove new
     * app/controllers/allergies_controller.rb
       - remove new, edit, update
       - extend parent's create and destroy
     * spec/controllers/patient_data_child_controller_spec.rb
       - add tests for generated identifiers

--- app/controllers/allergies_controller.rb
+++ app/controllers/allergies_controller.rb
@@ -1,41 +1,17 @@
 class AllergiesController < PatientDataChildController
-  layout false
-  
-  # TODO: Need a way to nil out the end_event through the web ui
 
-  def new
-    @allergy = Allergy.new
-    render :action => 'edit'
-  end
-
-  def edit
-    @allergy = @patient_data.allergies.find(params[:id])
-  end
-  
   def create
-    @allergy = Allergy.new(params[:allergy])
-    @patient_data.allergies << @allergy
+    super
     @patient_data.update_attribute(:no_known_allergies, false)
-    
-  end
-  
-  def update
-    @allergy = @patient_data.allergies.find(params[:id])
-    @allergy.update_attributes(params[:allergy])
-    
-    render :partial => 'show', :locals => {:allergy => @allergy,
-                                           :patient_data => @patient_data}
   end
   
   def destroy
-    allergy = @patient_data.allergies.find(params[:id])
-    allergy.destroy
+    super
     
     if @patient_data.allergies.empty?
       render :partial => "no_known_allergies_link", :locals=>{:patient_data=>@patient_data}
     else    
       render :partial => 'delete.rjs'
     end
-    
   end
 end
