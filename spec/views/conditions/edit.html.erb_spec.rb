3367aadd50657424d9e8e3515ca2040e7c4e2d1a
Zack Hobson <zack@opensourcery.com>
2009-04-09 14:01:19 -0700

    Fix the conditions/edit view spec.

--- spec/views/conditions/edit.html.erb_spec.rb
+++ spec/views/conditions/edit.html.erb_spec.rb
@@ -1,17 +1,18 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "conditions/_edit.html.erb" do
+describe "conditions/edit.html.erb" do
   fixtures :users
 
   describe "with an existing condition (conditions/edit)" do
     before do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @condition = Condition.create!(:patient_data => @patient_data)
+      assigns[:condition] = @condition
+      assigns[:patient_data] = @patient_data
     end
 
     it "should render the edit form with method PUT" do
-      render :partial  => 'conditions/edit', :locals => {:condition => @condition,
-                                                         :patient_data => @patient_data}
+      render 'conditions/edit'
       response.should have_tag("form[action=#{patient_data_instance_condition_path(@patient_data,@condition)}]") do
         with_tag "input[name=_method][value=put]"
       end
@@ -22,11 +23,12 @@ describe "conditions/_edit.html.erb" do
     before do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @condition = Condition.new
+      assigns[:condition] = @condition
+      assigns[:patient_data] = @patient_data
     end
 
     it "should render the edit form with method POST" do
-      render :partial  => 'conditions/edit', :locals => {:condition => @condition,
-                                                         :patient_data => @patient_data}
+      render 'conditions/edit'
       response.should have_tag("form[action=#{patient_data_instance_conditions_path(@patient_data)}][method=post]") do
         without_tag "input[name=_method][value=put]"
       end
