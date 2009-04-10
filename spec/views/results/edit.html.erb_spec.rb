8e297bdf32aea6ae6841cd3efb9dcb4554566bba
Zack Hobson <zack@opensourcery.com>
2009-04-10 10:44:36 -0700

    Spec and cleanup of ResultsController, SF ticket #2748115
     * spec/controllers/results_controller_spec.rb
       - spec suite for ResultsController
     * app/views/results/_create.html.erb -> app/views/results/create.html.erb
     * app/views/results/_delete.rjs -> app/views/results/destroy.rjs
     * app/views/results/_edit.html.erb -> app/views/results/edit.html.erb
     * spec/views/results/_edit.html.erb_spec.rb -> spec/views/results/edit.html.erb_spec.rb
       - renamed partials to templates
     * app/controllers/results_controller.rb
       - code and documentation cleanup
    
    https://sourceforge.net/tracker/?func=detail&aid=2748115&group_id=205665&atid=1126379

--- spec/views/results/edit.html.erb_spec.rb
+++ spec/views/results/edit.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "results/_edit.html.erb" do
+describe "results/edit.html.erb" do
   fixtures :users
 
   before do
@@ -11,11 +11,12 @@ describe "results/_edit.html.erb" do
     before do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @result = Result.create!(:patient_data => @patient_data)
+      assigns[:patient_data] = @patient_data
+      assigns[:result] = @result
     end
 
     it "should render the edit form with method PUT" do
-      render :partial  => 'results/edit', :locals => {:result => @result,
-                                                         :patient_data => @patient_data}
+      render 'results/edit'
       response.should have_tag("form[action=#{patient_data_instance_result_path(@patient_data,@result, :is_vital_sign => true)}]") do
         with_tag "input[name=_method][value=put]"
       end
@@ -26,11 +27,12 @@ describe "results/_edit.html.erb" do
     before do
       @patient_data = PatientData.create!(:name => 'foo', :user => User.find(:first))
       @result = Result.new
+      assigns[:patient_data] = @patient_data
+      assigns[:result] = @result
     end
 
     it "should render the edit form with method POST" do
-      render :partial  => 'results/edit', :locals => {:result => @result,
-                                                         :patient_data => @patient_data}
+      render 'results/edit'
       response.should have_tag("form[action=#{patient_data_instance_results_path(@patient_data, :is_vital_sign => true)}][method=post]") do
         without_tag "input[name=_method][value=put]"
       end
