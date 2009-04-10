8f8afa6fcd72fffae3d459454d7ffb3864fa35a2
Zack Hobson <zack@opensourcery.com>
2009-04-10 14:42:31 -0700

    Specs for UsersController#destroy action.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,46 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UsersController do
+  describe "in use by an admin" do
+    before do
+      @admin = stub_model(User, :administrator? => true)
+      controller.stub!(:current_user).and_return(@admin)
+    end
+
+    it "should permit deletion of other users" do
+      @user = stub_model(User, :display_name => '')
+      @user.should_receive(:destroy)
+      User.stub!(:find).and_return(@user)
+      delete :destroy, :id => '1'
+    end
+
+    it "should not permit deletion of self" do
+      @admin.should_not_receive(:destroy)
+      User.stub!(:find).and_return(@admin)
+      delete :destroy, :id => '1'
+    end
+  end
+
+  describe "in use by a non-admin" do
+    before do
+      @operator = stub_model(User, :administrator? => false)
+      controller.stub!(:current_user).and_return(@operator)
+    end
+
+    it "should not permit deletion of other users" do
+      @user = stub_model(User, :display_name => '')
+      @user.should_not_receive(:destroy)
+      User.stub!(:find).and_return(@user)
+      delete :destroy, :id => '1'
+    end
+
+    it "should not permit deletion of self" do
+      @operator.should_not_receive(:destroy)
+      User.stub!(:find).and_return(@operator)
+      delete :destroy, :id => '1'
+    end
+  end
+
   describe "handling GET /users" do
 
     before(:each) do
@@ -14,32 +54,28 @@ describe UsersController do
       controller.stub!(:current_user).and_return(@user)
     end
   
-    def do_get
-      get :index
-    end
-  
     it "should be successful" do
-      do_get
+      get :index
       response.should be_success
     end
 
     it "should render index template" do
-      do_get
+      get :index
       response.should render_template('index')
     end
   
     it "should find all users" do
       User.should_receive(:find).with(:all).and_return(@users)
-      do_get
+      get :index
     end
   
     it "should find user-specific vendors" do
       @user.should_receive(:vendors).and_return(@vendors)
-      do_get
+      get :index
     end
 
     it "should assign the found users for the view" do
-      do_get
+      get :index
       assigns[:users].should == @users
     end
   end
@@ -52,22 +88,18 @@ describe UsersController do
       controller.stub!(:current_user).and_return(@user)
     end
   
-    def do_get
-      get :edit, :id => "1"
-    end
-
     it "should be successful" do
-      do_get
+      get :edit, :id => "1"
       response.should be_success
     end
   
     it "should render edit template" do
-      do_get
+      get :edit, :id => "1"
       response.should render_template('edit')
     end
   
     it "should assign the found User for the view" do
-      do_get
+      get :edit, :id => "1"
       assigns[:user].should equal(@user)
     end
   end
@@ -75,30 +107,25 @@ describe UsersController do
   describe "handling PUT /users/1" do
 
     before(:each) do
-      @user = mock_model(User, :to_param => "1", :administrator? => false, :vendors => [])
+      @user = mock_model(User, :to_param => "1", :administrator? => false, :vendors => [], :update_attributes => true)
       User.stub!(:find).and_return(@user)
       controller.stub!(:current_user).and_return(@user)
     end
     
     describe "with successful update" do
 
-      def do_put
-        @user.should_receive(:update_attributes).and_return(true)
-        put :update, :id => "1", :user => {:first_name => 'Alex'}
-      end
-
       it "should update the found user" do
-        do_put
+        put :update, :id => "1", :user => {:first_name => 'Alex'}
         assigns(:user).should equal(@user)
       end
 
       it "should assign the found user for the view" do
-        do_put
+        put :update, :id => "1", :user => {:first_name => 'Alex'}
         assigns(:user).should equal(@user)
       end
 
       it "should redirect to the user" do
-        do_put
+        put :update, :id => "1", :user => {:first_name => 'Alex'}
         response.should redirect_to(edit_user_url("1"))
       end
 
@@ -106,13 +133,8 @@ describe UsersController do
 
     describe "with changing the password" do
         
-      def do_put
-        @user.should_receive(:update_attributes).and_return(true)
-        put :update, :id => "1", :user => {:password => '123456', :password_confirmation => '123456'}
-      end
-
       it "should update the user" do
-        do_put
+        put :update, :id => "1", :user => {:password => '123456', :password_confirmation => '123456'}
         assigns(:user).should equal(@user)
       end
 
@@ -120,13 +142,9 @@ describe UsersController do
     
     describe "with failed update" do
 
-      def do_put
+      it "should re-render 'edit'" do
         @user.should_receive(:update_attributes).and_return(false)
         put :update, :id => "1"
-      end
-
-      it "should re-render 'edit'" do
-        do_put
         response.should render_template('edit')
       end
 
