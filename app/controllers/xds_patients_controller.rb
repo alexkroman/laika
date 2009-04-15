28e2e94c883a946bbf403ba41bacb4b14226c6dd
Alex Kroman <alex@opensourcery.com>
2009-04-14 17:19:30 -0700

    Move some of the code in the xds patients query into the controller

--- app/controllers/xds_patients_controller.rb
+++ app/controllers/xds_patients_controller.rb
@@ -24,6 +24,8 @@ class XdsPatientsController < ApplicationController
     @metadata = rsqr.execute
     @vendors = current_user.vendors + Vendor.unclaimed
     @patient_identifier = pi
+    @vendor_test_plan = VendorTestPlan.new( :user_id => current_user.id, :metadata => @metadata)
+    @query = Kind.find_by_name('Query and Retrieve').id
   end
   
   # Creates the form that collects data for a provide and register test
